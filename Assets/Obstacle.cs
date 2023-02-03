0caa11874b85642908ad22ccea8ee27b91abff90
김 상완 <tkddhks7733@naver.com>
2023-02-03 20:02:34 +0900

    feat : obstacle logic 추가

--- Assets/Obstacle.cs
+++ Assets/Obstacle.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+    public float Force, radius;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            Debug.Log("으엥");
-            collision.gameObject.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(100,0,0), ForceMode.Impulse);
+            Knockback();
+        }
+    }
+
+    private void Knockback()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            Rigidbody rigid = colliders[i].GetComponent<Rigidbody>();
+
+            if (rigid != null)
+            {
+                Debug.Log("충돌 발동!");
+                rigid.AddExplosionForce(Force, transform.position, radius);
+                Destroy(gameObject);
+            }
         }
     }
 }
