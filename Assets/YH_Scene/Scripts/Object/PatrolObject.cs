032bbc2c08fe2944babb0b7dc8d52c831ea71b89
Yunho <bigunit3030@gmail.com>
2023-02-09 16:16:36 +0900

    refactor : Patrol Object 코드 수정

--- Assets/YH_Scene/Scripts/Object/PatrolObject.cs
+++ Assets/YH_Scene/Scripts/Object/PatrolObject.cs
@@ -5,72 +5,20 @@ using UnityEngine;
 public class PatrolObject : MonoBehaviour
 {
     [Header("Patrol")]
-    [SerializeField] private bool isHorizontal = true;
     [SerializeField] private float distance = 10.0f;
-    [SerializeField] private float speed = 10.0f;
+    [SerializeField] private float speed = 1.0f;
 
-    private bool isForward = true;
     private Vector3 startPosition;
 
-    private void Awake()
+    private void Start()
     {
         startPosition = transform.position;
-        if (isHorizontal)
-            transform.position += Vector3.right;
-        else
-            transform.position += Vector3.forward;
     }
 
     private void Update()
     {
-        if (isHorizontal)
-        {
-            if (isForward)
-            {
-                if (transform.position.x < startPosition.x + distance)
-                {
-                    transform.position += Vector3.right * speed * Time.deltaTime;
-                }
-
-                else
-                    isForward = false;
-            }
-
-            else
-            {
-                if (transform.position.x > startPosition.x)
-                {
-                    transform.position -= Vector3.right * speed * Time.deltaTime;
-                }
-
-                else
-                    isForward = true;
-            }
-        }
-
-        else
-        {
-            if (isForward)
-            {
-                if (transform.position.z < startPosition.z + distance)
-                {
-                    transform.position += Vector3.forward * speed * Time.deltaTime;
-                }
-
-                else
-                    isForward = false;
-            }
-
-            else
-            {
-                if (transform.position.z > startPosition.z)
-                {
-                    transform.position -= Vector3.forward * speed * Time.deltaTime;
-                }
-
-                else
-                    isForward = true;
-            }
-        }
+        Vector3 pos = startPosition;
+        pos.x = distance * Mathf.Sin(Time.time * speed);
+        transform.position = pos;
     }
 }
