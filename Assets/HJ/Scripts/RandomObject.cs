f2881f04dd519e53d350e58ac80cc662bf8a0e86
bigunit3030@gmail.com <bigunit3030@gmail.com>
2023-02-11 14:39:05 +0900

    feat : Obstacle Prefab 추가 및 수정

--- Assets/HJ/Scripts/RandomObject.cs
+++ Assets/HJ/Scripts/RandomObject.cs
@@ -47,15 +47,18 @@ public class RandomObject : MonoBehaviour
         for (int i = 0; i < objectList.Count * 0.5f; i++)
         {
             int rand = Random.Range(0, 2);
-            Transform targetDoorway = objectList[i * 2 + rand];
-            targetDoorway.gameObject.isStatic = true;
-            Transform[] childDoor = targetDoorway.GetComponentsInChildren<Transform>();
+            Transform targetDoorSet = objectList[i * 2 + rand];
+
+            targetDoorSet.GetComponent<Rigidbody>().isKinematic = true;
+/*            Transform[] childDoor = targetDoorway.GetComponentsInChildren<Transform>();
+
 
             for (int j = 0; j < childDoor.Length; j++)
             {
                 childDoor[j].gameObject.isStatic = true;
                 Debug.Log("static Door : " + childDoor[j]);
             }
+*/
             Debug.Log("static : " + objectList[i * 2 + rand]);
         }
     }
