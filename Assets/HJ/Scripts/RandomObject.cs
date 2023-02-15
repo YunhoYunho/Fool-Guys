6a6b7da8ab0741233039f965eab4a4fa252e511a
Yunho <bigunit3030@gmail.com>
2023-02-15 16:16:31 +0900

    fix : git 최신화

--- Assets/HJ/Scripts/RandomObject.cs
+++ Assets/HJ/Scripts/RandomObject.cs
@@ -1,30 +1,42 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
-using UnityEngine.UIElements;
 
-public class RandomObject : MonoBehaviour
+public class RandomObject : MonoBehaviourPun ,IPunObservable
 {
     public List<Transform> objectList;
-    
+
     public enum Type { Step, Door }
 
     public Type type;
 
+    private PhotonView pv;
+
     private void Awake()
+    {
+        pv = GetComponent<PhotonView>();
+    }
+
+    private void Start()
     {
         for (int i = 0; i < transform.childCount; i++)
         {
             objectList.Add(transform.GetChild(i));
         }
+        StartCoroutine(DelayInit());
+    }
 
+    IEnumerator DelayInit()
+    {
+        yield return new WaitForSeconds(3f);
         switch (type)
         {
             case Type.Step:
                 SteppingObj();
                 break;
             case Type.Door:
+                SetKinematic();
                 DoorObj();
                 break;
             default:
@@ -32,7 +44,32 @@ public class RandomObject : MonoBehaviour
         }
     }
 
+
+    private void SetKinematic()
+    {
+        for (int index = 0; index < objectList.Count; index++)
+        {
+            Transform[] trans = objectList[index].GetComponentsInChildren<Transform>();
+            foreach (Transform tf in trans)
+            {
+                tf.GetComponent<Rigidbody>().isKinematic = true;
+            }
+        }
+    }
+
+    [PunRPC]
     private void SteppingObj()
+    {
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            pv.RPC("TriggerOnFunc", RpcTarget.All);
+        }
+        
+    }
+
+    [PunRPC]
+    private void TriggerOnFunc()
     {
         for (int i = 0; i < objectList.Count * 0.5f; i++)
         {
@@ -42,24 +79,33 @@ public class RandomObject : MonoBehaviour
         }
     }
 
+
     private void DoorObj()
     {
-        for (int i = 0; i < objectList.Count * 0.5f; i++)
+
+        if (PhotonNetwork.IsMasterClient)
         {
-            int rand = Random.Range(0, 2);
-            Transform targetDoorSet = objectList[i * 2 + rand];
-
-            targetDoorSet.GetComponent<Rigidbody>().isKinematic = true;
-/*            Transform[] childDoor = targetDoorway.GetComponentsInChildren<Transform>();
-
-
-            for (int j = 0; j < childDoor.Length; j++)
-            {
-                childDoor[j].gameObject.isStatic = true;
-                Debug.Log("static Door : " + childDoor[j]);
-            }
-*/
-            Debug.Log("static : " + objectList[i * 2 + rand]);
+            pv.RPC("KinematicOffFunc", RpcTarget.All);
         }
+
+        
+    }
+
+    [PunRPC]
+    private void KinematicOffFunc()
+    {
+        int rand = Random.Range(0, 4);
+        Transform targetDoorSet = objectList[rand];
+        targetDoorSet.GetComponentsInChildren<Transform>();
+        targetDoorSet.GetComponent<Rigidbody>().isKinematic = false;
+        foreach (Transform tf in targetDoorSet)
+        {
+            tf.GetComponent<Rigidbody>().isKinematic = false;
+        }
+    }
+
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
     }
 }
